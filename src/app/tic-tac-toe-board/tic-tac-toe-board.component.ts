1f680d4ca6897e0b110967b7fd47c60cee12058a
brianbarker92 <brianbarker92@gmail.com>
2021-01-23 14:32:11 -0700

    Main board

---
+++ src/app/tic-tac-toe-board/tic-tac-toe-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tic-tac-toe-board',
+  templateUrl: './tic-tac-toe-board.component.html',
+  styleUrls: ['./tic-tac-toe-board.component.css']
+})
+export class TicTacToeBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
